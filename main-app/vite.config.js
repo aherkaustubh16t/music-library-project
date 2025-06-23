007880e0a07ade8319e72ed184409b820f15bb07
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-06-23 20:46:14 +0530

    updated

--- main-app/vite.config.js
+++ main-app/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     federation({
       name: 'mainApp',
       remotes: {
-        musicApp: 'https://music-library-project-qhqe.vercel.app/remoteEntry.js', // ✅ REMOVE 'musicApp@'
+        musicApp: 'https://music-library-project-7c57.vercel.app/remoteEntry.js',
       },
       shared: ['react', 'react-dom']
     }),
